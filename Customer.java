18fc333a8a86736c42cfa53e6848c8cc9230835a
Rennan Cordeiro <rennancl@gmail.com>
2019-11-10 16:58:18 -0300

    Commit #3

--- Customer.java
+++ Customer.java
@@ -12,8 +12,28 @@ class Customer {
    public String getName (){
       return _name;
    };
-  
-  public String statement() {
+   public double amountFor(Rental each){
+      double thisAmount = 0;
+         //determine amounts for each line
+         switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+               thisAmount += 2;
+               if (each.getDaysRented() > 2)
+                  thisAmount += (each.getDaysRented() - 2) * 1.5;
+               break;
+            case Movie.NEW_RELEASE:
+               thisAmount += each.getDaysRented() * 3;
+               break;
+            case Movie.CHILDRENS:
+               thisAmount += 1.5;
+               if (each.getDaysRented() > 3)
+                  thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+         }
+         return thisAmount;
+   }
+
+   public String statement() {
      double totalAmount = 0;
      int frequentRenterPoints = 0;
      Enumeration rentals = _rentals.elements();
@@ -21,23 +41,7 @@ class Customer {
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
